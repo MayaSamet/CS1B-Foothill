bc7763c01e1016dbc26e6d04928855f830648333
Maya Samet <mayasamet@gmail.com>
2021-04-19 13:15:32 -0700

    experiment with variables as parameters

---
+++ greetings/src/greetings.java
@@ -0,0 +1,19 @@
+class world
+{
+    public static void main(String[] args) {
+        //object #1
+        world Spanish = new world();
+        Spanish.greetings("Hola");   //Value for the word variable goes here!!!!
+    }
+
+    public void greetings(String word)
+    {
+        int letters = 4;
+        char firstletter = 'H';
+        double pi = 3.14;
+        System.out.println(word);
+        System.out.println(letters);
+        System.out.println(firstletter);
+        System.out.println(pi);
+    }
+}

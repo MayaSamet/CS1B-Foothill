1f03c93f1be8b07b3a7b37abc8935b09ac3c63aa
Maya Samet <mayasamet@gmail.com>
2021-04-19 11:56:47 -0700

    create separate method for println

--- HelloWorld/src/com/example/helloworld/HelloWorld.java
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -2,6 +2,14 @@ package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        hello();
     }
+
+    public static void hello()
+    {
+        System.out.println("***************");
+        System.out.println("* Hello World *");
+        System.out.println("***************");
+    }
+
 }

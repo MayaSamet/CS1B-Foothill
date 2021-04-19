835f2531e217fd7fed60624eb5ff7004b7fb12df
Maya Samet <mayasamet@gmail.com>
2021-04-19 13:22:35 -0700

    reorg - practice folder

--- Practice/src/HelloWorld/src/com/example/helloworld/HelloWorld.java
+++ Practice/src/HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.example.helloworld;
+package HelloWorld.src.com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
--- Practice/src/greetings/src/greetings.java
+++ Practice/src/greetings/src/greetings.java
@@ -1,3 +1,5 @@
+package greetings.src;
+
 class world
 {
     public static void main(String[] args) {

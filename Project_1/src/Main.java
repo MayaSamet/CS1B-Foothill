49cd88fff997b67d28f0d9a43409caeaf8bedc3c
Maya Samet <mayasamet@gmail.com>
2021-04-20 15:14:29 -0700

    copy constructor and preliminary number generation method

--- Project_1/src/Main.java
+++ Project_1/src/Main.java
@@ -1,9 +1,15 @@
+/**
+ *
+ * @author MayaSamet
+ */
+
 public class Main {
     public static void main(String[] args) {
         // create 3 objects of the driverLicense class using both overloaded constructors
-        austinLicense = ;
-        alyssaLicense = ;
-        almaLicense = ;
+        driverLicense austinLicense = new driverLicense("CA");
+        System.out.println(austinLicense.driverLicenseNumber);
+        // alyssaLicense = ;
+        // almaLicense = ;
 
         // call all mutator and accessor methods for testing purposes
 
--- Project_1/src/driverLicense.java
+++ Project_1/src/driverLicense.java
@@ -1,12 +1,46 @@
+/**
+ *
+ * @author MayaSamet
+ */
+
 public class driverLicense {
+    // *** variables ***
     // instance variables
+    String name;
+    public String address;
+    String height;
+    int weight;
+    String hairColor;
+    String eyeColor;
+    String driverLicenseNumber;
 
     // static variable
     private static int licenseCount = 1000;
 
-    // constructors
+    // *** constructors ***
+    // copy constructor
+    public driverLicense(driverLicense license){
+        this.name = license.name;
+        this.address = license.address;
+        this.height = license.height;
+        this.weight = license.weight;
+        this.hairColor = license.hairColor;
+        this.eyeColor = license.eyeColor;
+        this.driverLicenseNumber = license.driverLicenseNumber;
+    }
+
+    // 2 overloaded constructors
+    public driverLicense(String address){
+        numberGenerator(address);
+        this.address = address;
+    }
+
 
     // numberGenerator method
+    public void numberGenerator(String address){
+        String state = address.substring(Math.max(address.length() - 2, 0));
+        this.driverLicenseNumber = state + licenseCount;
+    }
 
     // accessor and mutator methods
 }

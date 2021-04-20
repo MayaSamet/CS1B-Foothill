64ff773e5a38f14fcf601effc19c974a1e8e054b
Maya Samet <mayasamet@gmail.com>
2021-04-20 14:42:31 -0700

    project template

---
+++ Project_1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // create 3 objects of the driverLicense class using both overloaded constructors
+        austinLicense = ;
+        alyssaLicense = ;
+        almaLicense = ;
+
+        // call all mutator and accessor methods for testing purposes
+
+        // create a copy of one of the objects using the copy constructor
+
+    }
+}
---
+++ Project_1/src/driverLicense.java
@@ -0,0 +1,12 @@
+public class driverLicense {
+    // instance variables
+
+    // static variable
+    private static int licenseCount = 1000;
+
+    // constructors
+
+    // numberGenerator method
+
+    // accessor and mutator methods
+}

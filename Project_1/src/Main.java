6d1bfb5a32cde15dcf8ec2d97d8a37169c8cfdab
Maya Samet <mayasamet@gmail.com>
2021-04-20 20:59:50 -0700

    call all accessor methods

--- Project_1/src/Main.java
+++ Project_1/src/Main.java
@@ -7,36 +7,33 @@ public class Main {
     public static void main(String[] args) {
         // create 3 objects of the driverLicense class using both overloaded constructors
         driverLicense austinLicense = new driverLicense(
-                "Lenoir City, TN",
+                "Knoxville, TN",
                 "Austin McEver",
                 "6\'2\"",
                 203);
-        System.out.println(austinLicense.driverLicenseNumber+austinLicense.weight);
 
         driverLicense alyssaLicense = new driverLicense(
-                "New York, NY",
+                "1337 Mystery Ave, New York, NY",
                 "Alyssa Takahashi",
                 "Brown",
                 "Black");
-        System.out.println(alyssaLicense.driverLicenseNumber+alyssaLicense.eyeColor+alyssaLicense.height);
 
-        driverLicense almaLicense = new driverLicense("3272 Smooth Pebble St, Palo Alto, CA 94303",
-                "Alma Morales",
-                "Blue",
+        driverLicense almaLicense = new driverLicense(
+                "4321 Smooth Pebble St, Palo Alto, CA 94303",
+                "Alma Yehezkely",
+                "Brown",
                 "Red");
-        System.out.println(almaLicense.driverLicenseNumber+almaLicense.eyeColor+almaLicense.name);
-
-
-        // create an object using the copy constructor
-        driverLicense alyssaLicenseCopy = new driverLicense(alyssaLicense);
-        System.out.println(alyssaLicenseCopy.driverLicenseNumber
-                + alyssaLicenseCopy.eyeColor
-                + alyssaLicenseCopy.hairColor);
-        // almaLicense = ;
-
-        // call all mutator and accessor methods for testing purposes
 
         // create a copy of one of the objects using the copy constructor
+        driverLicense alyssaLicenseCopy = new driverLicense(alyssaLicense);
+
+        // call all mutator and accessor methods for testing purposes
+        austinLicense.dataPrinter();
+        alyssaLicense.dataPrinter();
+        almaLicense.dataPrinter();
+        alyssaLicenseCopy.dataPrinter();
+
 
     }
+
 }
--- Project_1/src/driverLicense.java
+++ Project_1/src/driverLicense.java
@@ -6,14 +6,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class driverLicense {
+
     // *** variables ***
     // instance variables - initialize them with defaults
     String name = "Molly Baloney";
-    public String address = "12345 El Monte Road, Los Altos Hills, CA 12345";
+    public String address = "12345 El Monte Road, Los Altos Hills, CA 94022";
     String height = "5\'8\"";
-    int weight = 130;
-    String hairColor = "Pink";
-    String eyeColor = "Brown";
+    int weight = 140;
+    String hairColor = "Brown";
+    String eyeColor = "Green";
     String driverLicenseNumber = "None";
 
     // static variable
@@ -22,47 +23,105 @@ public class driverLicense {
     // *** constructors ***
     // copy constructor
     public driverLicense(driverLicense license){
-        this.name = license.name;
-        this.address = license.address;
-        this.height = license.height;
-        this.weight = license.weight;
-        this.hairColor = license.hairColor;
-        this.eyeColor = license.eyeColor;
-        this.driverLicenseNumber = license.driverLicenseNumber;
+        setName(license.name);
+        setAddress(license.address);
+        setHeight(license.height);
+        setWeight(license.weight);
+        setHairColor(license.hairColor);
+        setEyeColor(license.eyeColor);
+        setDriverLicenseNumber(license.driverLicenseNumber);
     }
 
     // 2 overloaded constructors
-    public driverLicense(String address, String name, String height, int weight){
+    public driverLicense(String address,
+                         String name,
+                         String height,
+                         int weight){
         numberGenerator(address);
-        this.address = address;
-        this.name = name;
-        this.height = height;
-        this.weight = weight;
+        setAddress(address);
+        setName(name);
+        setHeight(height);
+        setWeight(weight);
         licenseCount++;
     }
 
-    public driverLicense(String address, String name, String eyeColor, String hairColor){
+    public driverLicense(String address,
+                         String name,
+                         String eyeColor,
+                         String hairColor){
         numberGenerator(address);
-        this.address = address;
-        this.name = name;
-        this.eyeColor = eyeColor;
-        this.hairColor = hairColor;
+        setAddress(address);
+        setName(name);
+        setEyeColor(eyeColor);
+        setHairColor(hairColor);
         licenseCount++;
     }
 
 
     // numberGenerator method
     public void numberGenerator(String address){
-        //String state = address.substring(Math.max(address.length() - 2, 0));
-        //this.driverLicenseNumber = state + licenseCount;
-
         // match state initials by looking for 2 capital letters surrounded by a word boundary
         Pattern p = Pattern.compile("(\\b[A-Z]{2}\\b)");
         Matcher m = p.matcher(address);
         if (m.find()) {
-            this.driverLicenseNumber = m.group(1) + licenseCount;
+           setDriverLicenseNumber(m.group(1) + licenseCount);
         }
     }
 
+    // method calling accessor and mutator for testing purposes
+    public void dataPrinter(){
+        System.out.println("***************************************************");
+        System.out.println("Driver License Data for " + this.getName());
+        System.out.println("Address: " + this.getAddress());
+        System.out.println("Height: " + this.getHeight());
+        System.out.println("Weight: " + this.getWeight());
+        System.out.println("Hair Color: " + this.getHairColor());
+        System.out.println("Eye Color: " + this.getEyeColor());
+        System.out.println("Driver License Number: " + this.getDriverLicenseNumber());
+        System.out.println("***************************************************");
+    }
+
     // accessor and mutator methods
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getHeight() {
+        return height;
+    }
+    public void setHeight(String height) {
+        this.height = height;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public String getHairColor() {
+        return hairColor;
+    }
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+    public String getEyeColor() {
+        return eyeColor;
+    }
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+    public String getDriverLicenseNumber() {
+        return driverLicenseNumber;
+    }
+    public void setDriverLicenseNumber(String driverLicenseNumber) {
+        this.driverLicenseNumber = driverLicenseNumber;
+    }
 }

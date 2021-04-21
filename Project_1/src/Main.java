f700edb8ce00166c0cd9316f6e40eede070967dc
Maya Samet <mayasamet@gmail.com>
2021-04-20 19:33:28 -0700

    find state initials with regular expression

--- Project_1/src/Main.java
+++ Project_1/src/Main.java
@@ -6,11 +6,32 @@
 public class Main {
     public static void main(String[] args) {
         // create 3 objects of the driverLicense class using both overloaded constructors
-        driverLicense austinLicense = new driverLicense("CA", "Austin McEver", 203);
+        driverLicense austinLicense = new driverLicense(
+                "Lenoir City, TN",
+                "Austin McEver",
+                "6\'2\"",
+                203);
         System.out.println(austinLicense.driverLicenseNumber+austinLicense.weight);
 
-        driverLicense alyssaLicense = new driverLicense("CA", "Alyssa Takahashi", "Brown");
-        System.out.println(alyssaLicense.driverLicenseNumber+alyssaLicense.eyeColor);
+        driverLicense alyssaLicense = new driverLicense(
+                "New York, NY",
+                "Alyssa Takahashi",
+                "Brown",
+                "Black");
+        System.out.println(alyssaLicense.driverLicenseNumber+alyssaLicense.eyeColor+alyssaLicense.height);
+
+        driverLicense almaLicense = new driverLicense("3272 Smooth Pebble St, Palo Alto, CA 94303",
+                "Alma Morales",
+                "Blue",
+                "Red");
+        System.out.println(almaLicense.driverLicenseNumber+almaLicense.eyeColor+almaLicense.name);
+
+
+        // create an object using the copy constructor
+        driverLicense alyssaLicenseCopy = new driverLicense(alyssaLicense);
+        System.out.println(alyssaLicenseCopy.driverLicenseNumber
+                + alyssaLicenseCopy.eyeColor
+                + alyssaLicenseCopy.hairColor);
         // almaLicense = ;
 
         // call all mutator and accessor methods for testing purposes
--- Project_1/src/driverLicense.java
+++ Project_1/src/driverLicense.java
@@ -2,17 +2,19 @@
  *
  * @author MayaSamet
  */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class driverLicense {
     // *** variables ***
-    // instance variables
-    String name;
-    public String address;
-    String height;
-    int weight;
-    String hairColor;
-    String eyeColor;
-    String driverLicenseNumber;
+    // instance variables - initialize them with defaults
+    String name = "Molly Baloney";
+    public String address = "12345 El Monte Road, Los Altos Hills, CA 12345";
+    String height = "5\'8\"";
+    int weight = 130;
+    String hairColor = "Pink";
+    String eyeColor = "Brown";
+    String driverLicenseNumber = "None";
 
     // static variable
     private static int licenseCount = 1000;
@@ -30,27 +32,36 @@ public class driverLicense {
     }
 
     // 2 overloaded constructors
-    public driverLicense(String address, String name, int weight){
+    public driverLicense(String address, String name, String height, int weight){
         numberGenerator(address);
         this.address = address;
         this.name = name;
+        this.height = height;
         this.weight = weight;
         licenseCount++;
     }
 
-    public driverLicense(String address, String name, String eyeColor){
+    public driverLicense(String address, String name, String eyeColor, String hairColor){
         numberGenerator(address);
         this.address = address;
         this.name = name;
         this.eyeColor = eyeColor;
+        this.hairColor = hairColor;
         licenseCount++;
     }
 
 
     // numberGenerator method
     public void numberGenerator(String address){
-        String state = address.substring(Math.max(address.length() - 2, 0));
-        this.driverLicenseNumber = state + licenseCount;
+        //String state = address.substring(Math.max(address.length() - 2, 0));
+        //this.driverLicenseNumber = state + licenseCount;
+
+        // match state initials by looking for 2 capital letters surrounded by a word boundary
+        Pattern p = Pattern.compile("(\\b[A-Z]{2}\\b)");
+        Matcher m = p.matcher(address);
+        if (m.find()) {
+            this.driverLicenseNumber = m.group(1) + licenseCount;
+        }
     }
 
     // accessor and mutator methods

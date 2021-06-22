7a8a9bb97811d979088a692e50450c1598dc21fb
Maya Samet <mayasamet@gmail.com>
2021-06-22 15:43:02 -0700

    project 2 skeleton

--- Projects/Project_2/src/Main.java
+++ Projects/Project_2/src/Main.java
@@ -6,6 +6,11 @@
 public class Main {
     public static void main(String[] args) {
         boundedStack myStack = new boundedStack();
-        myStack.gamePlay();
+        myStack.load();
+        myStack.pop();
+        // load again so that unload displays 50 total values
+        myStack.load();
+        myStack.unload();
+        // remember to paste output into txt and submit with .java files
     }
 }
--- Projects/Project_2/src/boundedStack.java
+++ Projects/Project_2/src/boundedStack.java
@@ -3,9 +3,22 @@
  * @author MayaSamet
  */
 
-public class boundedStack {
+import java.util.*;
 
-    public void gamePlay() {
-        System.out.println("***************************************************");
+public class boundedStack {
+    // make instance member ArrayDeque of length 50 to hold spells
+
+    public void load() {
+        // get user input in loop. After 50 values remove oldest value (test with 5 values)
     }
+
+    public void pop(ArrayDeque){
+        // display and remove the most recently added value from the ArrayDeque
+    }
+
+    public void unload(){
+        // loop to display and remove the most recently added value in the ArrayDeque until empty
+        // Should edisplay 50 total values (5 during testing)
+    }
+
 }

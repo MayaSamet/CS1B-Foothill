8adf3561afd6444caef71373ed739f51d7afd786
Maya Samet <mayasamet@gmail.com>
2021-06-22 16:13:04 -0700

    finished load method

--- Projects/Project_2/src/Main.java
+++ Projects/Project_2/src/Main.java
@@ -6,11 +6,13 @@
 public class Main {
     public static void main(String[] args) {
         boundedStack myStack = new boundedStack();
+
+        System.out.println("~** Welcome to Maji **~");
         myStack.load();
-        myStack.pop();
+        //myStack.pop();
         // load again so that unload displays 50 total values
-        myStack.load();
-        myStack.unload();
+        //myStack.load();
+        //myStack.unload();
         // remember to paste output into txt and submit with .java files
     }
 }
--- Projects/Project_2/src/boundedStack.java
+++ Projects/Project_2/src/boundedStack.java
@@ -6,19 +6,43 @@
 import java.util.*;
 
 public class boundedStack {
-    // make instance member ArrayDeque of length 50 to hold spells
+    // make instance member ArrayDeque<Double> to hold spells
+    ArrayDeque<Double> spellDeque = new ArrayDeque<Double>();
 
     public void load() {
         // get user input in loop. After 50 values remove oldest value (test with 5 values)
+        // Create an instance of the Scanner class
+        Scanner input = new Scanner(System.in);
+        Double spell = 0.0;
+
+        while (true) {
+            System.out.print("Enter spell potency (any non-Float to quit): ");
+            try {
+                spell = input.nextDouble();
+            } catch (Exception e) {
+                break;
+            }
+
+            System.out.println("Spell added with potency " + spell);
+            // REMEMBER TO CHANGE TO 49
+            if (spellDeque.size() > 4) {
+                Double lastSpell = spellDeque.getLast();
+                // REMEMBER TO CHANGE TO 50
+                System.out.println("Exceeded 5 spell limit, removing oldest spell with potency " + lastSpell);
+                spellDeque.removeLast();
+            }
+
+            spellDeque.push(spell);
+        }
     }
 
-    public void pop(ArrayDeque){
+    //public void pop(){
         // display and remove the most recently added value from the ArrayDeque
-    }
+    //}
 
-    public void unload(){
+    //public void unload(){
         // loop to display and remove the most recently added value in the ArrayDeque until empty
         // Should edisplay 50 total values (5 during testing)
-    }
+    //}
 
 }

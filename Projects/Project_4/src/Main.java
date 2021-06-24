6ffc66dece5cecb7ebe49c4fd460e58015f25849
Maya Samet <mayasamet@gmail.com>
2021-06-24 16:47:54 -0700

    Project 4 Complete

--- Projects/Project_4/src/Main.java
+++ Projects/Project_4/src/Main.java
@@ -2,5 +2,9 @@ import java.io.*;
 
 public class Main {
     public static void main(String [] args) {
+        Reverser poemReverser = new Reverser();
+        poemReverser.FileToStack("reversePoem.txt");
+        poemReverser.StackToFile("reversedReversePoem.txt");
+    }
 
 }
--- Projects/Project_4/src/Reverser.java
+++ Projects/Project_4/src/Reverser.java
@@ -5,32 +5,54 @@ import java.util.ArrayDeque;
 public class Reverser {
 
     // instantiate private object of the ArrayDeque class to hold the poem stack
-    private ArrayDeque<String> poemStack;
+    private ArrayDeque<String> poemDeque = new ArrayDeque<String>(100);
 
     public void FileToStack(String filename){
         try {
             // set reversePoem as the input stream
-            FileInputStream poem = new FileInputStream("reversePoem.txt");
+            FileInputStream poem = new FileInputStream(filename);
+            BufferedReader poemReader = new BufferedReader(new InputStreamReader(poem));
 
             // initialize line variable to hold each poem line
-            String line = "";
+            String line;
 
-            while((line = poem.readLine()) != null) {
+            // loop through to each line to read into a string, print, and push to the ArrayDeque
+            while((line = poemReader.readLine()) != null) {
                 System.out.println(line);
-                myStack.Push(line);
+                poemDeque.push(line);
             }
+
+            poemReader.close();
 
         } catch (FileNotFoundException e) {
             System.out.println("File not found exception");
+        } catch (IOException e) {
+            System.out.println("IO exception");
         }
 
     }
 
     public void StackToFile(String filename){
-        //write the contects of the ArrayDeque objec 
+        // write the contents of the poemDeque object to a new file, reversed
+        try{
+            PrintWriter poemOutput = new PrintWriter(filename);
+
+            String line;
+            while(!poemDeque.isEmpty()){
+                line = poemDeque.pop();
+                System.out.println(line);
+                poemOutput.println(line);
+            }
+
+            poemOutput.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found exception");
+        }
     }
 
-
 }
+
+
+
 
 

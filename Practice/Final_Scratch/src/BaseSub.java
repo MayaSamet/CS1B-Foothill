19f335d7a8dbf7407ec10b7e7cc839a415776a01
Maya Samet <mayasamet@gmail.com>
2021-06-23 17:26:46 -0700

    final practice

--- Practice/Final_Scratch/src/BaseSub.java
+++ Practice/Final_Scratch/src/BaseSub.java
@@ -1,2 +1,17 @@
-public class BaseSub {
+class Base {
+    void show()
+    {
+        System.out.println("Base");
+    }
 }
+
+// Inherited class
+class Sub extends Base {
+    // This method overrides show() of Parent
+    @Override
+    void show()
+    {
+        System.out.println("Sub");
+    }
+}
+  
--- Practice/Final_Scratch/src/Main.java
+++ Practice/Final_Scratch/src/Main.java
@@ -1,7 +1,11 @@
+
+
+import java.io.*;
 import java.util.*;
 
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Test GList (Q4)
         GList<Integer> score = new GList<Integer>(9);
         score.addFirst(1);
@@ -9,6 +13,43 @@ public class Main {
         //System.out.println(score);
 
         // Base + Sub (Q7)
+        Base b = new Base();
+        Sub s = new Sub();
+
+        Base bmirror = b;
+        Sub smirror = s;
+
+        bmirror.show();
+        smirror.show();
+
+        b = s;
+        s.show();
+        b.show();
+
+        // GalaxyData (Q20)
+
+
+        FileInputStream filey = new FileInputStream("test.txt");
+        char c1 = (char) filey.read();
+        char c2 = (char) filey.read();
+        char c3 = (char) filey.read();
+        char c4 = (char) filey.read();
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+
+        try{
+            char c5 = (char) filey.read();
+            System.out.println(c5);
+        } catch (Exception e){
+            System.out.println("Womp Wompwomp");
+        }
+
+        char c6 = (char) filey.read();
+        System.out.println(c6);
+
 
     }
 }
